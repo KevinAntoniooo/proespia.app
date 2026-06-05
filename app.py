130589b48afe0ffeb0f3db992076d8f009d2c3ff
KevinAntoniooo <kevin@proespia.cl>
2026-06-05 19:25:50 -0400

    fix: api_eventos_por_fecha filtra fallas para agenda admin

--- app.py
+++ app.py
@@ -1567,7 +1567,7 @@ def api_eventos_agenda():
 def api_eventos_por_fecha():
     try:
         fecha_str = request.args.get('fecha', '')
-        visitas = VisitaProgramada.query.all()
+        visitas = VisitaProgramada.query.filter(VisitaProgramada.falla_id.is_(None)).all()
         if fecha_str:
             visitas = [v for v in visitas if v.fecha_programada.strftime('%Y-%m-%d') == fecha_str]
         resultados = []
